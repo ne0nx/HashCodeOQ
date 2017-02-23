c14c967049a7eef971b593163a2b781cfe8417fd
Dmitriy Karachentsov <d.karachentsov@gmail.com>
2017-02-23 21:06:25 +0200

    Initial Commit

---
+++ HashCodeOQ/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HashCodeOQ
+//
+//  Created by Dmitriy Karachentsov on 23/2/17.
+//  Copyright © 2017 Dmitriy Karachentsov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
